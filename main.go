69c569795ca781570d1d634b16be7c15e9a569fe
bIgBV <bhargav@recruiterbox.com>
2015-12-18 18:36:11 +0530

    Start working on structs.

--- main.go
+++ main.go
@@ -1,48 +1,48 @@
 package main
 
 import (
-    "net/http"
-    "time"
-    "log"
+	"log"
+	"net/http"
+	"time"
 )
 
 type XavierResponse struct {
-    resp *http.Response
-    label string
-    latency int64
+	resp    *http.Response
+	label   string
+	latency time.Duration
 }
 
 type XavierConf struct {
-    serviceList *map[string]string
-    timeout int8
+	serviceList *map[string]string
+	timeout     int8
 }
 
 func serviceMonitor(label string) XavierResponse {
-    client := &http.Client{}
-    request, err := http.NewRequest("HEAD", "http://www.google.com", nil)
-    if err != nil {
-        log.Fatalln(err)
-    }
+	client := &http.Client{}
+	request, err := http.NewRequest("HEAD", "http://www.google.com", nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-    request.Header.Set("User-Agent", "Xavier monitoring spider v0.1")
+	request.Header.Set("User-Agent", "Xavier monitoring spider v0.1")
 
-    startTime := time.Now()
-    response, err := client.Do(request)
-    if err != nil {
-        log.Fatalln(err)
-    }
-    endTime := time.Now()
-    totalTime := endTime.Sub(startTime)
+	startTime := time.Now()
+	response, err := client.Do(request)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	endTime := time.Now()
+	totalTime := endTime.Sub(startTime)
 
-    return XavierResponse {
-        response,
-        label,
-        int64(totalTime/time.Millisecond),
-    }
+	return XavierResponse{
+		response,
+		label,
+		totalTime,
+	}
 }
 
-func main () {
-    response := serviceMonitor("Google")
+func main() {
+	response := serviceMonitor("Google")
 
-    log.Println(response.resp.StatusCode, ":", response.label, ":", response.latency, "ms")
+	log.Println(response.resp.StatusCode, ":", response.label, ":", response.latency, "ms")
 }
